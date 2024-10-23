6354e1dd4a793aab5353a1b987fc6c351e981bc2
Syed-Subtain <syed.subtain@apimatic.io>
2024-10-23 07:37:04 +0000

    Automated commit message

--- src/main/java/com/maxio/advancedbilling/AdvancedBillingClient.java
+++ src/main/java/com/maxio/advancedbilling/AdvancedBillingClient.java
@@ -97,7 +97,7 @@ public final class AdvancedBillingClient implements Configuration {
 
     private static final CompatibilityFactory compatibilityFactory = new CompatibilityFactoryImpl();
 
-    private static String userAgent = "AB SDK Java:6.5.6 on OS {os-info}";
+    private static String userAgent = "AB SDK Java:6.6.6 on OS {os-info}";
 
     /**
      * Current API environment.
